303f0bd9e869e4653633d8d1e945ebfc791513c1
Rohit Patil <rt1823r@gmail.com>
2026-08-23 21:30:17 +0530

    fix: check authentication

--- frontend/src/app/middleware.tsx
+++ frontend/src/app/middleware.tsx
@@ -9,9 +9,7 @@ function Middleware({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     if (typeof window === "undefined") return;
     const token = localStorage.getItem("token");
-    if (token) {
-      store.dispatch(fetchUser(token));
-    }
+    store.dispatch(fetchUser(token || ""))
   }, []);
   return <Provider store={store}>{children}</Provider>;
 }
